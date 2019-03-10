ecb456498e2a77c423ba4ea4fc249efeea3153e7
Björn Friedrichs <bjoern@friedrichs1.de>
2019-03-10 23:08:10 +0000

    Better parsing

--- routes/__init__.py
+++ routes/__init__.py
@@ -10,3 +10,5 @@ def setup_routes(app):
     app.router.add_get('/', hello)
     app.router.add_get('/forum', forum.index)
     app.router.add_get('/forum/{forum_id}', forum.list_threads)
+
+    app.router.add_get('/thread/{thread_id}', thread.list_comments)
