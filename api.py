9aab4d9fa122fe4afd1a251d3e3599a6d8e1cca0
Björn Friedrichs <bjoern@friedrichs1.de>
2019-04-14 00:45:22 +0100

    ssl

--- api.py
+++ api.py
@@ -2,6 +2,9 @@ import asyncio
 from aiohttp import web
 from routes import setup_routes
 from middlewares import compress_response
+import ssl
+import yaml
+import os
 
 async def on_startup(app):
     pass
@@ -19,4 +22,11 @@ async def create_app():
     return app
 
 if __name__ == "__main__":
-    web.run_app(create_app(), host='127.0.0.1', port=80)
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+        sslcontext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        sslcontext.load_cert_chain(os.path.join(cfg['ssl']['path'], 'cert.pem'), os.path.join(cfg['ssl']['path'], 'privkey.pem'))
+        web.run_app(create_app(), host='127.0.0.1', port=443)
+
+    # docker run -d --name resetera-api-2 -e 'LETSENCRYPT_EMAIL=bjoern@friedrichs1.de' -e 'LETSENCRYPT_HOST=resetera.bjoern-friedrichs.de' -e 'VIRTUAL_HOST=resetera.bjoern-friedrichs.de' bfriedrichs/api
